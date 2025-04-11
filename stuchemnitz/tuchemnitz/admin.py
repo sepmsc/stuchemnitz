c589f624bd76ef5f361a8438fed79d82c50dfa10
Sepp <se.saz1921@gmail.com>
2025-04-11 23:46:51 +0200

    add admin

--- stuchemnitz/tuchemnitz/admin.py
+++ stuchemnitz/tuchemnitz/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'comment', 'created_at')
+    search_fields = ('name', 'email')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
