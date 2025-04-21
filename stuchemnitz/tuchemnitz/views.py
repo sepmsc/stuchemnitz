5ac70c158313cab382c1b152bfe7ec779f4784b7
Sepp <se.saz1921@gmail.com>
2025-04-21 23:29:32 +0200

    add submissions

--- stuchemnitz/tuchemnitz/views.py
+++ stuchemnitz/tuchemnitz/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from tuchemnitz.forms import SubmitForm
+# from tuchemnitz.forms import SubmitForm
 
 # Create your views here.
 def home(request):
@@ -24,14 +24,14 @@ def news(request):
     return render(request, 'news.html')
 
 def conversations(request):
+    return render(request, 'conversations.html')
+
+def submitview(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         comment = request.POST.get('comment')
-        # Save the comment to the database or perform any other action
-        # For example, you can create a new Comment object and save it
-        # comment_obj = Comments(name=name, email=email, comment=comment)
-        # comment_obj.save()
-        # Redirect to the same page or another page after saving
-        return render(request, 'conversations.html', {'success': True})
-    return render(request, 'conversations.html')
+
+        request.save()
+        return render(request, 'submit.html', {'name': name, 'email': email, 'comment': comment})
+    
